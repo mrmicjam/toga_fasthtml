99e23677ee26c8e35a0f943e6561f5a683f255f8
Micah Jamison <dataconcise@gmail.com>
2025-05-28 17:17:26 -0700

    add example of message passing between webserver and toga app

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
+import asyncio
+import queue
+from threading import Thread
+
+import daphne
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-import daphne
-import asyncio
-from threading import Thread
+
+from fasthtml_app.main import app, msg_queue
+
+
+def worker():
+    while True:
+        print("blocking for queue get")
+        try:
+            event = msg_queue.get(True, None)
+        except queue.Empty:
+            pass
+        else:
+            print("got item")
+            print(event)
+            the_app.handle_event(event)
 
 
 class FastHTMLApp(toga.App):
     def web_server(self):
-        from fasthtml_app.main import app
-        from daphne.server import Server
         from daphne.endpoints import build_endpoint_description_strings
+        from daphne.server import Server
 
         endpoints = build_endpoint_description_strings(
             host="127.0.0.1",
-            port=0  # dynamic port allocation
+            port=0,  # dynamic port allocation
         )
         endpoints = sorted(endpoints)
 
@@ -24,6 +40,9 @@ class FastHTMLApp(toga.App):
         # the runloop
         self.server.run()
 
+    def handle_event(self, event):
+        self.webview.url = "https://dataconcise.com"
+
     def startup(self):
         """Construct and show the Toga application.
 
@@ -31,6 +50,10 @@ class FastHTMLApp(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
+        worker_thread = Thread(target=worker)
+        worker_thread.daemon = True  # makes it so c thread ends when python does
+        worker_thread.start()
+
         self.webview = toga.WebView(
             on_webview_load=self.on_webview_loaded, style=Pack(flex=1)
         )
@@ -52,7 +75,6 @@ class FastHTMLApp(toga.App):
 
         self.on_exit = self.cleanup
 
-
     async def on_running(self):
         await self.server_exists
 
@@ -63,12 +85,13 @@ class FastHTMLApp(toga.App):
         self.webview.url = f"http://{host}:{port}/"
         self.main_window.show()
 
-    def on_webview_loaded(self, widget):
-        ...
+    def on_webview_loaded(self, widget): ...
 
 
 def main():
     return FastHTMLApp("FastHTML Toga Example", "com.dataconcise.fasthtml_toga")
 
+
 if __name__ == "__main__":
-    main().main_loop()
+    the_app = main()
+    the_app.main_loop()
--- fasthtml_app/main.py
+++ fasthtml_app/main.py
@@ -1,8 +1,18 @@
-from fasthtml.common import fast_app, H1
+import queue
+
+from fasthtml.common import H1, A, Div, fast_app
 
 app, rt = fast_app()
 
+
+msg_queue = queue.Queue()
+
+
 @app.get("/")
 def homepage():
-    return H1("Hello from FastHTML")
+    return Div(H1("Hello from FastHTML"), A("hello", href="/hello"))
 
+
+@app.get("/hello")
+def hello():
+    msg_queue.put(1)
